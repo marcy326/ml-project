9bc32751cf5e3da810c4b3b230006b67e4bbf3e1
marcy <marc326marcy@gmail.com>
2024-02-26 21:59:17 +0900

    Update: run.py実行時のパラメータを指定

--- run.py
+++ run.py
@@ -9,12 +9,14 @@ parameters = parameters["mlflow"]
 TRACKING_URI = parameters["TRACKING_URI"]
 EXPERIMENT_NAME = parameters["EXPERIMENT_NAME"]
 PROJECT_URI = parameters["PROJECT_URI"]
+VERSION = parameters["VERSION"]
 
 @click.command()
 @click.option('--tracking_uri', default=TRACKING_URI)
 @click.option('--experiment_name', default=EXPERIMENT_NAME)
 @click.option('--project_uri', default=PROJECT_URI)
-def main(tracking_uri, experiment_name, project_uri):
+@click.option('--version', default=VERSION)
+def main(tracking_uri, experiment_name, project_uri, version):
     mlflow.set_tracking_uri(tracking_uri)
     experiment = mlflow.get_experiment_by_name(experiment_name)
     if experiment is None:  # 当該Experiment存在しないとき、新たに作成
@@ -22,7 +24,7 @@ def main(tracking_uri, experiment_name, project_uri):
     else: # 当該Experiment存在するとき、IDを取得
         experiment_id = experiment.experiment_id
 
-    run = mlflow.projects.run(uri=project_uri, env_manager="virtualenv", experiment_id=experiment_id)
+    run = mlflow.projects.run(uri=project_uri, env_manager="virtualenv", version=version, experiment_id=experiment_id)
 
 if __name__ == "__main__":
     main()
