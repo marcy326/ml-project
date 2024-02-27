052b67c7e8d0d7c6b4704b5ff6ff1f4a3718b539
marcy <marc326marcy@gmail.com>
2024-02-27 14:48:28 +0900

    Add env_magaer parameter

--- run.py
+++ run.py
@@ -16,7 +16,8 @@ VERSION = parameters["VERSION"]
 @click.option('--experiment_name', default=EXPERIMENT_NAME)
 @click.option('--project_uri', default=PROJECT_URI)
 @click.option('--version', default=VERSION)
-def main(tracking_uri, experiment_name, project_uri, version):
+@click.option('--env_manager', default="virtualenv")
+def main(tracking_uri, experiment_name, project_uri, version, env_manager):
     mlflow.set_tracking_uri(tracking_uri)
     experiment = mlflow.get_experiment_by_name(experiment_name)
     if experiment is None:  # 当該Experiment存在しないとき、新たに作成
@@ -24,7 +25,7 @@ def main(tracking_uri, experiment_name, project_uri, version):
     else: # 当該Experiment存在するとき、IDを取得
         experiment_id = experiment.experiment_id
 
-    run = mlflow.projects.run(uri=project_uri, env_manager="virtualenv", version=version, experiment_id=experiment_id)
+    run = mlflow.projects.run(uri=project_uri, env_manager=env_manager, version=version, experiment_id=experiment_id)
 
 if __name__ == "__main__":
     main()
